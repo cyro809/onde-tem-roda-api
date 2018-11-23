d174f07ef1071123297c278d5ff271d3ad000b8e
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2018-11-23 18:19:27 -0200

    Creating database table scripts.

---
+++ database/create.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	database, _ := sql.Open("sqlite3", "./ondetemroda.db")
+
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, nome TEXT, email TEXT, telefone TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare(`
+		CREATE TABLE IF NOT EXISTS event (
+			id INTEGER PRIMARY KEY,
+			nome_evento TEXT,
+			grupo TEXT,
+			responsavel TEXT,
+			responsavel_tel TEXT,
+			responsavel_email TEXT,
+			endereco TEXT,
+			place_id TEXT,
+			user_id INTEGER NOT NULL,
+				FOREIGN KEY (user_id) REFERENCES users(id)
+		)
+	`)
+	statement.Exec()
+}
