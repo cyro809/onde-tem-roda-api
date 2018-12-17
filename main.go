9fd0a0ecaee897a27613a26f781a6005f0f8f1e0
Cyro Guimaraes <cyro809@gmail.com>
2018-12-17 16:48:12 -0200

    Adds select user by email and delete user by ID.

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/onde-tem-roda-api/models"
-
 	"github.com/onde-tem-roda-api/manager"
 )
 
@@ -13,12 +11,13 @@ import (
 // criar um pacote cmd com um main para instrumentar os command lines
 func main() {
 	// user := manager.SelectUserByID(1)
-	user := models.User{
-		Nome:     "Brutus",
-		Email:    "brutus@brutus.com",
-		Telefone: "11111111",
-	}
-	manager.InsertUser(user)
-	users := manager.SelectAllUsers()
-	fmt.Println(users)
+	// user := models.User{
+	// 	Nome:     "Brutus",
+	// 	Email:    "brutus@brutus.com",
+	// 	Telefone: "11111111",
+	// }
+	// manager.InsertUser(user)
+	user := manager.SelectUserByEmail("cyro809@gmail.com")
+	// users := manager.SelectAllUsers()
+	fmt.Println(user)
 }
--- manager/user.go
+++ manager/user.go
@@ -11,16 +11,37 @@ import (
 
 // SelectUserByID selects user from the database by its ID
 func SelectUserByID(ID int) models.User {
-	fmt.Println("selecting")
 	user := models.User{}
 	db, err := database.OpenDB()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	rows, err2 := db.Query(fmt.Sprintf("SELECT * FROM users WHERE ID = %d", ID))
-	if err2 != nil {
-		fmt.Println(err2)
+	rows, err := db.Query(fmt.Sprintf("SELECT * FROM users WHERE ID = %d", ID))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	// Verificar o uso de .Get ou Select
+	for rows.Next() {
+		rows.Scan(&user.ID, &user.Nome, &user.Email, &user.Telefone)
+	}
+
+	return user
+}
+
+// SelectUserByEmail selects user from the database by its e-mail
+func SelectUserByEmail(email string) models.User {
+	user := models.User{}
+	db, err := database.OpenDB()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rows, err := db.Query(fmt.Sprintf(`SELECT * FROM users WHERE Email = "%s"`, email))
+	if err != nil {
+		fmt.Println(err)
 	}
 	defer db.Close()
 
@@ -41,9 +62,9 @@ func SelectAllUsers() []models.User {
 		fmt.Println(err)
 	}
 
-	rows, err2 := db.Query("SELECT * FROM users")
-	if err2 != nil {
-		fmt.Println(err2)
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Println(err)
 	}
 	defer db.Close()
 
@@ -67,4 +88,24 @@ func InsertUser(user models.User) {
 	statement, err := db.Prepare("INSERT INTO users (Nome, Email, Telefone) VALUES (?, ?, ?)")
 
 	statement.Exec(user.Nome, user.Email, user.Telefone)
+}
+
+// DeleteUserByID deletes user from database by its ID
+func DeleteUserByID(ID int) {
+	db, err := database.OpenDB()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	statement, err := db.Prepare("DELETE FROM users WHERE id = ?")
+
+	res, err := statement.Exec(ID)
+
+	affec, err := res.RowsAffected()
+
+	if affec > 0 {
+		fmt.Println(fmt.Printf("Affected %d rows", affec))
+	} else {
+		fmt.Println("No rows were affected")
+	}
 }
