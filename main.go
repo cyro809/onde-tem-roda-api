6d18894aaa0aeb2891cd46afb1c5af47b9710d97
Cyro Guimaraes <cyro809@gmail.com>
2019-01-16 10:23:03 -0200

    Adds httprouter dependency.

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/onde-tem-roda-api/manager"
+	"github.com/julienschmidt/httprouter"
 )
 
 //olhar pacote flag
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
 // criar um pacote cmd com um main para instrumentar os command lines
 func main() {
-
+	router := httprouter.New()
+	router.GET("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
 	// event := models.Event{
 	// 	NomeEvento:       "Roda Mister Capoeira",
 	// 	Grupo:            "Mister Capoeira Grupo",
@@ -24,8 +32,8 @@ func main() {
 	// manager.UpdateEventByField(2, "NomeEvento", "Mr Caopeira Roda")
 	// manager.DeleteEventByID(2)
 	// manager.InsertEvent(event)
-	events := manager.SelectAllEvents()
-	event := manager.SelectEventByID(1)
+	// events := manager.SelectAllEvents()
+	// event := manager.SelectEventByID(1)
 	// user := models.User{
 	// 	Nome:     "Brutus",
 	// 	Email:    "brutus@brutus.com",
@@ -34,11 +42,11 @@ func main() {
 
 	// manager.UpdateUserByField(1, "Telefone", "777777777")
 	// manager.InsertUser(user)
-	user := manager.SelectUserByEmail("cyro809@gmail.com")
+	// user := manager.SelectUserByEmail("cyro809@gmail.com")
 
 	// users := manager.SelectAllUsers()
 
-	fmt.Println(user)
-	fmt.Println(events)
-	fmt.Println(event)
+	// fmt.Println(user)
+	// fmt.Println(events)
+	// fmt.Println(event)
 }
